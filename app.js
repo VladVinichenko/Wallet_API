0183b2bc5de074d676934313e818b3353190843d
Andrii Hrytsai <andrey.hrytsai@solverlabs.com>
2022-05-15 01:09:36 +0300

    add auth endpoints

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const path = require("path")
-
+const swaggerUi = require("swagger-ui-express");
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth")
-
+const swaggerDocument = require("./swagger.json");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -17,12 +17,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/contacts", contactsRouter);
 
-
 app.use("/link", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/link.html"))
 })
 
 app.use("/auth", authRouter);
+app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerDocument)
+    );
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
