7a7bd9a276d944b2019d1b513a0d5e77d6487d31
DeadNord <eosipopo@gmail.com>
2022-05-16 17:56:05 +0300

    clear

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -9,17 +9,6 @@ const validationMiddleware = schema => async (req, res, next) => {
     });
     next(error);
   }
-
-  // return (req, res, next) => {
-  //   const { error } = schema.validate(req.body);
-  //   if (error) {
-  //     res.status(400).json({
-  //       status: error.details,
-  //       code: 400,
-  //     });
-  //   }
-  //   next();
-  // };
 };
 
 module.exports = validationMiddleware;
