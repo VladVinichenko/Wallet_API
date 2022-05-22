45f7bdf8192ce893df3ddcefd98245f0692ae03a
DeadNord <eosipopo@gmail.com>
2022-05-22 22:06:28 +0300

    fix signOut

--- src/controllers/auth/signOutController.js
+++ src/controllers/auth/signOutController.js
@@ -1,11 +1,23 @@
 const { User } = require('../../models/index');
+const { Unauthorized } = require('http-errors');
 
 const signOutController = async (req, res, next) => {
   const { refreshToken } = req.cookies;
+
+  if (!refreshToken) {
+    return res.status(401).json(Unauthorized('Not authorized'));
+  }
+
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    return res.status(401).json(Unauthorized('Not authorized'));
+  }
+
   await User.findOneAndUpdate(refreshToken, {
     accessToken: null,
     refreshToken: null,
   });
+
   res.clearCookie('refreshToken');
   return res.status(204).json();
 };
