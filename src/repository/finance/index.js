0b2df91085aab745aa33008d7dce427c22e42504
vladislav-slobodenjuk <vladjun@gmail.com>
2022-05-22 18:26:12 +0300

    fix number row

--- src/repository/finance/index.js
+++ src/repository/finance/index.js
@@ -22,7 +22,7 @@ async function getTotalValue(user) {
     return acc;
   }, []);
 
-  const Months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const Months = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
 
   return { user: data[0], uniqueYears, Months };
 }
