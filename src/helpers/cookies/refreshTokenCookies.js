d62cb81b405875a3bdbf89e640356516e8489f7e
DeadNord <eosipopo@gmail.com>
2022-05-23 11:35:17 +0300

    Update refreshTokenCookies.js

--- src/helpers/cookies/refreshTokenCookies.js
+++ src/helpers/cookies/refreshTokenCookies.js
@@ -1,7 +1,6 @@
 const dayjs = require('dayjs');
 
 const addRefreshTokenCookies = (res, refreshToken) => {
-  // console.log();
   return res.cookie('refreshToken', refreshToken, {
     expires: dayjs().add(1, 'days').toDate(),
     httpOnly: true,
