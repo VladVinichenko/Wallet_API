55ff4b48b482e545522e00fd290829085fd5cfab
DeadNord <eosipopo@gmail.com>
2022-05-25 01:58:17 +0300

    Update cookiesSave.js

--- src/helpers/cookies/cookiesSave.js
+++ src/helpers/cookies/cookiesSave.js
@@ -5,7 +5,7 @@ const cookiesSave = async (res, key, data) => {
     expires: dayjs().add(1, 'h').toDate(),
     // httpOnly: true,
     // signed: true,
-    // secure: process.env.NODE_ENV !== 'development',
+    secure: process.env.NODE_ENV !== 'development',
     sameSite: process.env.NODE_ENV !== 'development' && 'None',
   });
 };
