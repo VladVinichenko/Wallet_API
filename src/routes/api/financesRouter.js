f99e4e4762e4f1ae971b0a36e8ad50515d66fdab
DeadNord <eosipopo@gmail.com>
2022-05-24 22:05:13 +0300

    add midlware

--- src/routes/api/financesRouter.js
+++ src/routes/api/financesRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const { ctrlWrapper } = require('../../middlewares/index');
+const { authMiddleware, ctrlWrapper } = require('../../middlewares/index');
 
 const { getCoursesCtrl } = require('../../controllers/finances');
 
 const router = express.Router();
 
-router.get('/courses', ctrlWrapper(getCoursesCtrl));
+router.get('/courses', authMiddleware, ctrlWrapper(getCoursesCtrl));
 
 module.exports = router;
