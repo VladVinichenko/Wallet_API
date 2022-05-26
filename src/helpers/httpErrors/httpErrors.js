354a08406d85c7416df16c83c49f7909fb42c391
DeadNord <eosipopo@gmail.com>
2022-05-26 22:03:45 +0300

    base code

---
+++ src/helpers/httpErrors/httpErrors.js
@@ -0,0 +1,51 @@
+const { Unauthorized, Conflict, NotFound } = require('http-errors');
+
+const conflictSwitch = type => {
+  switch (type) {
+    case 'emailExist':
+      Conflict(`User with ${email} already exist`);
+      break;
+
+    default:
+      break;
+  }
+};
+// Conflict(`User with ${email} already exist`);
+
+const unauthorizedSwitch = type => {
+  switch (type) {
+    case 'base':
+      Unauthorized('Not authorized');
+      break;
+    case 'emailNotFound':
+      Unauthorized(`Email ${email} not found`);
+      break;
+    case 'emailNotVerified':
+      Unauthorized(`Email ${email} not verified`);
+      break;
+    case 'wrongPass':
+      Unauthorized(`Password wrong`);
+      break;
+
+    default:
+      break;
+  }
+};
+// const notAuthorized = Unauthorized('Not authorized');
+// Unauthorized(`Email ${email} not found`);
+// Unauthorized(`Email ${email} not verified`);
+// Unauthorized(`Password wrong`);
+
+const notFoundSwitch = type => {
+  switch (type) {
+    case 'emailNotFound':
+      NotFound('User not found');
+      break;
+
+    default:
+      break;
+  }
+};
+// NotFound('User not found');
+
+module.exports = { conflictSwitch, unauthorizedSwitch, notFoundSwitch };
--- src/services/auth/getVerifyService.js
+++ src/services/auth/getVerifyService.js
@@ -1,9 +1,10 @@
 const { findUserByVerif, verifyUser } = require('../../repository/auth');
+const { NotFound } = require('http-errors');
 
 const getVerifyService = async verificationToken => {
   const user = await findUserByVerif(verificationToken);
   if (!user) {
-    throw new Error('User not found');
+    throw NotFound('User not found');
   }
 
   await verifyUser(verificationToken);
